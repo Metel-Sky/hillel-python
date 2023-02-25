5e9aac8e80bec606ed4149245306ec978f28bb98
Vinch <djkf1212@gmail.com>
2023-02-25 18:27:58 +0200

    Test

---
+++ lesson2.py
@@ -0,0 +1,32 @@
+a = 2
+print(a)
+
+b = 2.
+
+print(type(a), type(b))
+
+x = 7
+print(x - 2.0)
+
+x = 5
+print(x ** 2)
+x = 36
+print(pow(x, 1/2))
+
+x = 10
+print('x // 2')
+
+x = divmod(11, 2)
+print(x)
+
+x = 5
+print(type(x))
+
+x = 5 / 2
+print(type(x))
+
+import math
+print(round(math.pi, 2))
+
+print(int(3.9))
+
