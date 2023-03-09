9e6fbc7fed270a9bb10e0b81382f113d9d2fb143
Vinch <djkf1212@gmail.com>
2023-03-09 18:43:05 +0200

    homeWork5

--- Lesson_5/HomeWork_5.py
+++ Lesson_5/HomeWork_5.py
@@ -1,13 +1,13 @@
 
 total = 0
 while True:
-    user_input = input("Введите число или sum: ")
-    if user_input.lower() == "sum":
+    user_input = input("Введите число или sum: ")#
+    if user_input.lower() == "sum":#
         break
 
     try:
         num = float(user_input)
-        total += num
+        total += num#
     except ValueError:
         print('Не корректный ввод')
 
@@ -19,15 +19,15 @@ print("Сумма введеных чисел:", total)
 
 total = []
 while True:
-    user_input = input("Введите число или sum: ")
-    if user_input.lower() == "sum":
-        break
-    if user_input.isdigit():
-        num = float(user_input)
-        total.append(num)
+    user_input = input("Введите число или sum: ")  # Предлогаем ввести число или sum
+    if user_input.lower() == "sum":  # Проверяем что введено и если (sum) выходим из цыкла
+        break#
+    if user_input.isdigit():# проверяем что ввел юзверь
+        num = float(user_input)#
+        total.append(num)#
     else:
-        print('Не корректный ввод')
-print("Сума введених чисел:", sum(total))
+        print('Не корректный ввод')#
+print("Сума введених чисел:", sum(total)) # При выходе из цыкла печатаем сумму чисел списка (total)
 
 
 #____________________________________________________________________________________________________________________
