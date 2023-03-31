418f69027de519e61df9c1288286125ab9b9e141
Олександр <djkif1212@gmail.com>
2023-03-31 12:09:33 +0300

    HomeWork8

---
+++ Lesson_8/HomeWork_notes.py
@@ -0,0 +1,43 @@
+
+# add - додати нотатку
+# shortest -  Від найкоротшої до найдовшої
+# longest - Від найдовшої до найкоротшоЇ
+# latest - Від найпізнішої до найранішої
+# earliest -Від найранішої до найпізнішої
+
+notes = []
+
+while True:
+    command = input("доступні команди: add, "
+                    "shortest,  longest, latest, earliest > ").lower()
+
+    if command == "add": # додаемо нотатку
+        note = input("Введіть нотатку: ")
+        notes.append(note)
+
+    elif command == "earliest":#
+        sorted_n = sorted(notes)
+        print("Від найранішої до найпізнішої:")
+        for note in sorted_n:
+            print(note)
+
+    elif command == "latest":#
+        sorted_n = sorted(notes, reverse=True)
+        print("Від найпізнішої до найранішої:")
+        for note in sorted_n:
+            print(note)
+
+    elif command == "longest":#
+        sorted_n = sorted(notes, key=len, reverse=True)
+        print("Від найдовшої до найкоротшоЇ:")
+        for note in sorted_n:
+            print(note)
+
+    elif command == "shortest":
+        sorted_n = sorted(notes, key=len)
+        print("Від найкоротшої до найдовшої:")
+        for note in sorted_n:
+            print(note)
+
+    else:
+        print("Невідома команда")
