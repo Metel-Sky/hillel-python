ea53d28f1c883d2c8e55089c4a246ef3b160a932
Олександр <djkif1212@gmail.com>
2023-04-03 13:56:57 +0300

    HomeWork_11

---
+++ Lesson_11/HomeWork_NotesWrite.py
@@ -0,0 +1,74 @@
+
+# add - додати нотатку
+# shortest -  Від найкоротшої до найдовшої
+# longest - Від найдовшої до найкоротшоЇ
+# latest - Від найпізнішої до найранішої
+# earliest - Від найранішої до найпізнішої
+# exit -
+
+
+def text_file_writeline(file_handler, text_to_write: list):
+    # записывает в файл список строк
+    # плюсы: сразу много текста
+    # минусы: не ставит перенос на новую строку
+    file_handler.writelines(text_to_write)
+
+
+def text_file_write(file_handler, text_to_write: str):
+    # записывает в файл просто строку
+    file_handler.write(text_to_write)
+
+    # искусственное создание ошибки
+    # Как сообщить об ошибке
+    # как сделать так, чтобы программа думала что у нее ошибка
+    # raise ValueError("Здесь произошло что-то не так")
+
+
+def notes():
+    with open('my_file', encoding='utf-8') as read_f:
+        notes_list = read_f.readlines()[:]
+
+    while True:
+        command = input("доступні команди: add, "
+                        "shortest,  longest, latest, earliest, exit > ").lower()
+
+        if command == "add":
+            note = input("Введіть нотатку: ")
+            notes_list.append(note)
+
+        elif command == "earliest":
+            sorted_notes = sorted(notes_list)
+            print("Від найранішої до найпізнішої:")
+            for note in sorted_notes:
+                print(note)
+
+        elif command == "latest":
+            sorted_notes = sorted(notes_list, reverse=True)
+            print("Від найпізнішої до найранішої:")
+            for note in sorted_notes:
+                print(note)
+
+        elif command == "longest":
+            sorted_notes = sorted(notes_list, key=len, reverse=True)
+            print("Від найдовшої до найкоротшої:")
+            for note in sorted_notes:
+                print(note)
+
+        elif command == "shortest":
+            sorted_notes = sorted(notes_list, key=len)
+            print("Від найкоротшої до найдовшої:")
+            for note in sorted_notes:
+                print(note)
+
+        elif command == "exit":
+            with open('my_file', 'a', encoding='utf-8') as f:
+                text_file_write(f, '\n')
+                for note in notes_list:
+                    text_file_write(f, note + '\n')
+            break
+
+        else:
+            print("Невідома команда")
+
+if __name__ == '__main__':
+    notes()
