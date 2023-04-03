1d70f24299dee25449fc439b27fd492505187ede
Олександр <djkif1212@gmail.com>
2023-04-03 13:57:20 +0300

    HomeWork_10.2

--- Lesson_8/HomeWork_notes.py
+++ Lesson_8/HomeWork_notes.py
@@ -4,40 +4,45 @@
 # longest - Від найдовшої до найкоротшоЇ
 # latest - Від найпізнішої до найранішої
 # earliest -Від найранішої до найпізнішої
+def notes():
+    notes = []
 
-notes = []
+    while True:
+        command = input("доступні команди: add, "
+                        "shortest,  longest, latest, earliest > ").lower()
 
-while True:
-    command = input("доступні команди: add, "
-                    "shortest,  longest, latest, earliest > ").lower()
+        if command == "add": # додаемо нотатку
+            note = input("Введіть нотатку: ")
+            notes.append(note)
 
-    if command == "add": # додаемо нотатку
-        note = input("Введіть нотатку: ")
-        notes.append(note)
+        elif command == "earliest":#
+            sorted_n = sorted(notes)
+            print("Від найранішої до найпізнішої:")
+            for note in sorted_n:
+                print(note)
 
-    elif command == "earliest":#
-        sorted_n = sorted(notes)
-        print("Від найранішої до найпізнішої:")
-        for note in sorted_n:
-            print(note)
+        elif command == "latest":#
+            sorted_n = sorted(notes, reverse=True)
+            print("Від найпізнішої до найранішої:")
+            for note in sorted_n:
+                print(note)
 
-    elif command == "latest":#
-        sorted_n = sorted(notes, reverse=True)
-        print("Від найпізнішої до найранішої:")
-        for note in sorted_n:
-            print(note)
+        elif command == "longest":#
+            sorted_n = sorted(notes, key=len, reverse=True)
+            print("Від найдовшої до найкоротшоЇ:")
+            for note in sorted_n:
+                print(note)
 
-    elif command == "longest":#
-        sorted_n = sorted(notes, key=len, reverse=True)
-        print("Від найдовшої до найкоротшоЇ:")
-        for note in sorted_n:
-            print(note)
+        elif command == "shortest":
+            sorted_n = sorted(notes, key=len)
+            print("Від найкоротшої до найдовшої:")
+            for note in sorted_n:
+                print(note)
 
-    elif command == "shortest":
-        sorted_n = sorted(notes, key=len)
-        print("Від найкоротшої до найдовшої:")
-        for note in sorted_n:
-            print(note)
 
-    else:
-        print("Невідома команда")
+
+        else:
+            print("Невідома команда")
+
+if __name__ == '__main__':
+    notes()
